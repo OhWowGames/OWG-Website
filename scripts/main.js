917f4e9ff4875b060420f3d66aeef2b1702746f2
Stu Sigrest <43093024+stusigrest@users.noreply.github.com>
2025-02-20 17:11:45 -0500

    Updated Homepage with a link gallery

--- scripts/main.js
+++ scripts/main.js
@@ -9,14 +9,17 @@ const urlParameter = new URLSearchParams(window.location.search);
 
 // Page Load
 document.addEventListener("DOMContentLoaded", function(e) {
+    // Copyright Year
     replaceText('year', new Date().getFullYear());
 
+    // Dynamic logo height
     function resizeLogo() {
-        var logoHeight = $(window).outerHeight() - ($('#footer').outerHeight() + $('h2').outerHeight()) - 150;
+        var logoHeight = $(window).outerHeight() - ($('#footer').outerHeight() + $('h2').outerHeight() + $('#link-gallery').outerHeight()) - 150;
         $('.logo-splash').height(logoHeight);
     }
     resizeLogo();
     
+    // Window resize listener
     $(window).on( "resize", function(e) {
         resizeLogo();
     } );
